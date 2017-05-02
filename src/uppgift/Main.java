abc0eabf95bf7e3277e5449d3186f83731a2d490
MaxFre <mafr@live.se>
2017-05-02 17:00:30 +0200

    as
    as

--- src/uppgift/Main.java
+++ src/uppgift/Main.java
@@ -3,8 +3,9 @@ package uppgift;
 
 public class Main {
 	public static void main(String[] args) {
-		Gui run = new Gui();		
-//		GuiBesökare run2 = new GuiBesökare();
+//		Gui run = new Gui();		
+		GuiBesökare run2 = new GuiBesökare();
+		
 		
 	}
 }
